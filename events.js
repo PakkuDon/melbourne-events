656697492a65ccf18c56ff18158a910fdd31be56
PakkuDon <pakku.pd@gmail.com>
2024-06-15 19:05:35 +1000

    Add some events for June 2024

---
+++ events.js
@@ -0,0 +1,63 @@
+// Available properties for events defined in https://fullcalendar.io/docs/event-object
+// Note that end date is exclusive
+/* Example events
+{
+  title: 'All day event',
+  start: '2024-06-14',
+  end: '2024-06-15',
+  description: 'Description',
+},
+
+{
+  title: 'Event with times included',
+  start: '2024-06-14T10:30:00+11:00',
+  end: '2024-06-15T11:30:00+11:00',
+  description: 'Description',
+},
+
+{
+  title: 'Recurring event',
+  startTime: '09:00:00+11:00',
+  endTime: '23:00:00+11:00',
+  startRecur: '2024-06-01',
+  endRecur: '2024-07-15',
+  description: 'Description',
+  daysOfWeek: [0, 1, 2, 3, 4, 5, 6],
+},
+*/
+
+const events = [
+  {
+    title: 'RISING: Melbourne',
+    start: '2024-06-01',
+    end: '2024-06-17',
+    description: 'https://2024.rising.melbourne/',
+  },
+  {
+    title: 'Lightscape',
+    start: '2024-06-14',
+    end: '2024-08-05',
+    description: 'https://www.lightscapeaustralia.com/city/melbourne.html',
+  },
+  {
+    title: 'Firelight Festival',
+    start: '2024-06-28',
+    end: '2024-07-01',
+    description: 'https://firelight.melbourne.vic.gov.au/festival/',
+  },
+  {
+    title: 'Firelight Labyrinth',
+    start: '2024-06-28',
+    end: '2024-07-15',
+    description: 'https://firelight.melbourne.vic.gov.au/labyrinth/',
+  },
+  {
+    title: 'Winter Night Market | Queen Victoria Market',
+    startTime: '17:00:00+11:00',
+    endTime: '22:00:00+11:00',
+    startRecur: '2024-06-05',
+    endRecur: '2024-08-29',
+    description: 'https://thenightmarket.com.au/',
+    daysOfWeek: [3]
+  },
+]
--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   let calendarElement = document.getElementById('calendar')
   let calendar = new FullCalendar.Calendar(calendarElement, {
     initialView: 'dayGridMonth',
-    events: [],
+    // events is defined in events.js and included in index.html
+    events: events,
   })
   calendar.render()
 })
