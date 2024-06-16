7bfbe90b73afb11cc52fec8516073a54b231ac51
PakkuDon <pakku.pd@gmail.com>
2024-06-16 17:08:38 +1000

    Display event title and description on click

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+let popoverElement
+
+const showEventDetails = (event) => {
+  popoverElement.hidePopover()
+  popoverElement.innerHTML = `
+    <h2>${event.title}</h2>
+    <p>
+      ${event.extendedProps.description}
+    </p>
+  `
+  popoverElement.showPopover()
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+  popoverElement = document.querySelector('#popover')
   let calendarElement = document.getElementById('calendar')
   let calendar = new FullCalendar.Calendar(calendarElement, {
     initialView: 'dayGridMonth',
     // events is defined in events.js and included in index.html
     events: events,
+    eventClick: function(info) {
+      showEventDetails(info.event)
+    },
   })
   calendar.render()
 })
