b472ea1a64369b4f884d39ad501871b0f6264625
PakkuDon <pakku.pd@gmail.com>
2025-09-16 23:18:14 +1000

    Add one-off script to list locations in dataset

---
+++ scripts/list-all-locations.js
@@ -0,0 +1,22 @@
+// One-off script to list all locations in data set
+import events from "../events.js";
+
+const locations = {};
+events.forEach((event) => {
+  const location = event.location;
+
+  if (!locations[location]) {
+    const locationDetails = {
+      address: location.address,
+    };
+    const googleMapsUrl = event.links.find(
+      (link) => link.label === "Google Maps",
+    )?.url;
+    if (googleMapsUrl) {
+      locationDetails.googleMapsUrl = googleMapsUrl;
+    }
+    locations[location.address] = locationDetails;
+  }
+});
+
+console.log(JSON.stringify(locations, null, 2));
