dd20ef41bc68b763ffa07ebfd109bb8b89a601f3
PakkuDon <pakku.pd@gmail.com>
2026-04-15 23:26:20 +1000

    Update end date for Book of Mormon

--- data/2026.js
+++ data/2026.js
@@ -122,7 +122,7 @@ const events = [
   {
     title: "The Book of Mormon",
     start: "2026-02-06",
-    end: "2026-06-01",
+    end: "2026-07-20",
     description: "",
     tags: ["Musical", "Theatre"],
     links: [
